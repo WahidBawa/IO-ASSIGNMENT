87e6a48b1cbf564856cecc11ccd19f620867a8c2
Wahid Bawa <31601021@student.publicboard.ca>
2018-04-06 12:43:42 -0400

    ATP DONE

--- fileAssignment1.py
+++ fileAssignment1.py
@@ -1,2 +1,12 @@
 #fileAssignment1.py
-print("test")
+from pprint import *
+low = int(input("ENTER THE POINTS FROM: "))
+high = int(input("ENTER THE POINTS TO: "))
+finalList = []
+people = open("ATP.txt", "r").read().strip().split("\n")
+for i in people:
+	person = i.split(',')
+	
+	if int(person[-1]) >= low and int(person[-1]) <= high:
+		finalList.append(person[0] + ', ' + person[2] + ', ' + person[-1])
+pprint(finalList)		
