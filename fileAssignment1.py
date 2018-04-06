4e1893d074e81608833a6c7df85a5b658578511d
Wahid Bawa <31601021@student.publicboard.ca>
2018-04-06 12:58:58 -0400

    FINSIHED QUESTION 1

--- fileAssignment1.py
+++ fileAssignment1.py
@@ -4,9 +4,8 @@ low = int(input("ENTER THE POINTS FROM: "))
 high = int(input("ENTER THE POINTS TO: "))
 finalList = []
 people = open("ATP.txt", "r").read().strip().split("\n")
+fname = open(str(low) + "to" + str(high) + ".txt", "w")
 for i in people:
 	person = i.split(',')
-	
 	if int(person[-1]) >= low and int(person[-1]) <= high:
-		finalList.append(person[0] + ', ' + person[2] + ', ' + person[-1])
-pprint(finalList)		
+		fname.write(person[0] + ', ' + person[2] + ', ' + person[-1] + "\n")
