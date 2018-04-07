48b37871e99ec1d6156130a76b7ac378c527b80c
Yoh8000 <wahidbawa10@gmail.com>
2018-04-06 21:55:57 -0400

    Q2 Added

---
+++ 2/fileAssignment2.py
@@ -0,0 +1,13 @@
+# fileAssignment2.py
+from pprint import *
+seuss = open("hamlet.txt", "r").read().strip().split('\n')
+words = []
+for i in seuss:
+	sentence = i.split(' ')
+	for n in sentence:
+		n = n.lower().strip('.').strip('?').strip('!').strip(',')#.strip('[').strip(']').strip('(').strip(')').strip(';')
+		if n != '':
+			if n not in words:
+				words.append(n)
+pprint(words)
+pprint(len(words))
